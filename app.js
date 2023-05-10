5db13dc5db47946a53935db6d0df0baf4d4940f8
Triinu <triinu.moppel@voco.ee>
2023-05-10 21:35:30 +0300

    Displayed city weather data from API

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ function weatherDataFetch(city){
     }) // Convert data to json
     .then(function(data) {
     console.log(data);
+    drawWeather(data);
     })
     .catch(function(error) {
     //catch any errors
@@ -14,4 +15,12 @@ function weatherDataFetch(city){
 }
 
 {weatherDataFetch('Tallinn');
+}
+
+function drawWeather(data) {
+    var celcius=Math.round(parseFloat(data.main.temp)-273.15);
+    var description=data.weather[0].description;
+    document.querySelector('#description').innerHTML = description;
+    document.querySelector('#temp').innerHTML=celcius+'&deg;';
+    document.querySelector('#location').innerHTML=data.name;
 }
